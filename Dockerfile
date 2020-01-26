4cb3132a10d0647dcdebbab9f6fc819bf83c08bf
philenius <philenius@users.noreply.github.com>
2020-01-26 18:14:54 +0100

    replace every 'fsg-scoring' by 'fsg-scores'

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,14 @@ FROM node:10-slim as builder
 RUN npm install -g @angular/cli@8.3.23
 
 WORKDIR /app
-COPY fsg-scoring/ .
+COPY fsg-scores/ .
 
 RUN npm install && ng build --prod
 
 FROM nginx:1.15
 
 COPY nginx.conf /etc/nginx/nginx.conf
-COPY --from=builder /app/dist/fsg-scoring/ /usr/share/nginx/html/
+COPY --from=builder /app/dist/fsg-scores/ /usr/share/nginx/html/
 RUN chown -R nginx /usr/share/nginx/html/
 
 EXPOSE 80
--- fsg-scores/src/app/app.component.spec.ts
+++ fsg-scores/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fsg-scoring'`, () => {
+  it(`should have as title 'fsg-scores'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('fsg-scoring');
+    expect(app.title).toEqual('fsg-scores');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('fsg-scoring app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('fsg-scores app is running!');
   });
 });
